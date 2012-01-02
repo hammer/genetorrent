2afa1dd5fcb0ccf1692dc94189e82aa8d532d36b
dnelson <dnelson@ce0a4a09-f1da-4806-a783-9435acac145d>
2012-01-02 19:43:28 +0000

    svn_clean.sh finished
    git-svn-id: svn+ssh://svn/srv/svnroot/annaisystems-genetorrent/newGeneTorrent/trunk@617 ce0a4a09-f1da-4806-a783-9435acac145d

--- svn_clean.sh
+++ svn_clean.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-
-#clear out any extended attributes that Finder may add
+# Clear out files left over from maintainer-clean
 
 cd libtorrent
+make maintainer-clean
 rm -f config.log config.report configure
-rm -f m4/libtool.m4 m4/lt~obsolete.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/ltoptions.m4
+rm -f m4/libtool.m4 m4/lt~obsolete.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/ltoptions.m4 aclocal.m4
 rm -fr autom4te.cache build-aux
 rm -f Makefile Makefile.in
 rm -f src/Makefile src/Makefile.in
@@ -18,4 +18,24 @@ chmod a-x docs/*.rst docs/*.htm* src/*.cpp include/libtorrent/*.hpp
 cd -
 
 
+cd GeneTorrent
+make maintainer-clean
+rm -f configure
+rm -f Makefile.in
+rm -f aclocal.m4
+rm -f src/Makefile.in
+rm -f src/config.h.in
+cd -
 
+
+cd scripts
+make maintainer-clean
+rm -f configure
+rm -f Makefile.in
+rm -f missing install-sh
+rm -f aclocal.m4
+cd -
+
+cd rpmbuild/SOURCES
+rm -rf GeneTorrent-[0-9].[0-9].[0-9].[0-9]*
+cd -
