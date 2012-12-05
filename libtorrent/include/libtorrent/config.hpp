3ec8d6ce1fa77169af55088756ff7877a26e8cff
Matt Lupfer <mlupfer@cardinalpeak.com>
2012-12-05 16:39:13 -0700

    Solaris: change TORRENT_ICONV_ARG to match expected signature

--- libtorrent/include/libtorrent/config.hpp
+++ libtorrent/include/libtorrent/config.hpp
@@ -204,6 +204,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #define TORRENT_COMPLETE_TYPES_REQUIRED 1
 #define TORRENT_USE_IFCONF 1
 #define TORRENT_HAS_SALEN 0
+#define TORRENT_ICONV_ARG (const char**)
 
 // ==== BEOS ===
 #elif defined __BEOS__ || defined __HAIKU__
